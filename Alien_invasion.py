1ba3502360146fb0c19edcf3ad86a1e451a46a95
liya2021 <77222664+liya2021@users.noreply.github.com>
2023-02-10 19:20:03 +0000

    helper method

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior.'''
 
@@ -20,17 +21,23 @@ class AlienInvasion:
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitime()
+    def _check_events(self):
+        '''Respond to keypresses and mouse events.'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        '''update images on the screen, and flip to the new sceen.'''
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitime()
+
+        pygame.display.flip()
+        # Make the most recently drawn screen visible.
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
